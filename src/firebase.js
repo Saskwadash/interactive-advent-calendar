8ad7c32ade954dabe0b7fba7a694beadcd7175e0
Saskwadash <104017340+Saskwadash@users.noreply.github.com>
2025-10-29 15:01:10 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,27 +1,14 @@
+// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyANfy17YjNp_Zh_TVZ-KBZQnrbWXtQ4vwM",
   authDomain: "advent-calendar-1219d.firebaseapp.com",
   projectId: "advent-calendar-1219d",
   storageBucket: "advent-calendar-1219d.firebasestorage.app",
   messagingSenderId: "350359114979",
-  appId: "1:350359114979:web:9e3c0c9f2c6584b0eae1d6",
-  measurementId: "G-DK0CZNPC26"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+  appId: "1:350359114979:web:9e3c0c9f2c6584b0eae1d6"
 };
 
 const app = initializeApp(firebaseConfig);
